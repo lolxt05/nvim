18a7e026be238ada125c37b8165ccbdfdc9ba125
suckdick <suckdick>
2025-03-14 23:27:51 +0100

    idk man i think install.sh finnished

--- install-nvim.sh
+++ install-nvim.sh
@@ -27,25 +27,30 @@ install_dependencies() {
 rust_update_or_install() {
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 }
+
 install_latest_nvim() {
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
   sudo rm -rf /opt/nvim
   sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 }
+
 install_ghostty() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
   cd ~/.config/ghostty/
   ghostty +show-config --default --docs
+  rm ../ghostty/config -rf
+  cp ~/.config/nvim/config ../ghostty/
 }
+
 clear_paths() {
   rm -rf ~/.local/share/nvim/
   rm -rf ~/.local/state/nvim/
   rm -rf ~/.config/nvim/lua-language-server
   sudo rm -rf /opt/nvim-linux-x86_64
 }
+
 move_config_files(){
   mv ~/.config/nvim/.bashrc ~/.bashrc
-
 }
 
 clear_and_clone_base() {
@@ -53,9 +58,15 @@ clear_and_clone_base() {
   install_latest_nvim
   rust_update_or_install
   install_ghostty
+  set_bashrc
+}
+
+set_bashrc() {
+  mv ~/.config/nvim/bashrc ~/.bashrc
 }
 
 main() {
+  [ "$(pwd)" = "$HOME/.config/nvim" ] || { echo "Error: Current directory is not ~/.config/nvim"; exit 1; }
   install_dependencies
   clear_and_clone_base
 }
