35fec3775b34455de12063274aa0e0a1f8c71a9c
Nikolai Fessl <nikolai.fessl@samariterbund.net>
2025-03-17 10:01:51 +0100

    added new bashrc custom

--- install-nvim.sh
+++ install-nvim.sh
@@ -60,6 +60,7 @@ clear_and_clone_base() {
 set_configs() {
   /bin/cp -rf ~/.config/nvim/bashrc ~/.bashrc
   /bin/cp -rf ~/.config/nvim/config ~/.config/ghostty/config
+  /bin/cp -rf ~/.config/nvim/bashrc_custom ~/.custom_bashrc
 }
 
 main() {
