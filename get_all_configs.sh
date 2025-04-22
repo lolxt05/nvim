41e95de146fe5a42203475957a62bbb69f818f3a
test <test>
2025-04-22 10:55:11 +0200

    new configs and bash rc configs

--- get_all_configs.sh
+++ get_all_configs.sh
@@ -1,5 +1,4 @@
 cp -f ~/.bashrc ~/.config/nvim/configs/bashrc
-cp -fr ~/.custom_bashrc ~/.config/nvim/configs/custom_bashrc
 cp -fr ~/.config/ghostty ~/.config/nvim/configs/ghostty
 cp -fr ~/.config/btop ~/.config/nvim/configs/btop
-
+cp -fr ~/.config/wallpaper.jpg ~/.config/nvim/wallpaper.jpg
