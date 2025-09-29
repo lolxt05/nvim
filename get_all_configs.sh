663b98cc9a6aff1e015b193b3c2c6ea9f4142274
test <test>
2025-09-29 09:17:05 +0200

    lazy vim beginn

--- get_all_configs.sh
+++
@@ -1,4 +0,0 @@
-cp -f ~/.bashrc ~/.config/nvim/configs/bashrc
-cp -fr ~/.config/ghostty ~/.config/nvim/configs/ghostty
-cp -fr ~/.config/btop ~/.config/nvim/configs/btop
-cp -fr ~/.config/wallpaper.jpg ~/.config/nvim/wallpaper.jpg
--- init.lua
+++ init.lua
@@ -1,87 +1,14 @@
--- From https://github.com/nvim-lua/kickstart.nvim -- commit e39a8bce9d25b3bd8d50098fadf13575a4a71b4b
-
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- [[ Install `lazy.nvim` plugin manager ]]
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-vim.opt.termguicolors = true
-
--- [[ Configure plugins ]]
--- NOTE: Here is where you install your plugins.
---  You can configure plugins using the `config` key.
---
---  You can also configure plugins after the setup call,
---    as they will be available in your neovim runtime.
-require('plugins-configure')
-
--- [[ Setting options ]]
-require('general-options')
-
--- [[ Basic Keymaps ]]
-require('mappings')
-
-require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
+vim.api.nvim_echo({
+  {
+    "Do not use this repository directly\n",
+    "ErrorMsg",
   },
-  view = {
-    width = 30,
+  {
+    "Please check the docs on how to get started with LazyVim\n",
+    "WarningMsg",
   },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
--- [[ Highlight on yank ]]
--- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
-})
+  { "Press any key to exit", "MoreMsg" },
+}, true, {})
 
--- [[ Configure Telescope ]]
-require('telescope-configure')
-
--- [[ Configure Treesitter ]]
-require('treesitter-configure')
-
--- [[ Configure LSP ]]
-require('lsp')
-
--- [[ Configure nvim-cmp ]]
-require('complete')
-
--- [[ Configure neorg ]]
-require('neorg-configure')
-
--- Customizations
-require('restore-position')
-require('autocommands')
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.fn.getchar()
+vim.cmd([[quit]])
